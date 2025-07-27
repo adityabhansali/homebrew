aff216a635bdcd20ef173bb8939e1d37d11e63b5
adityabhansali <adityabhansalijain@gmail.com>
2025-07-27 14:06:20 +0530

    added: gradio

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
+import gradio as gr
 from Models.gemini_ai import generate_text_with_conversation
 from functions import *
 from template import react_system_prompt
 
-def coffee_suggester_main():
-    prompt = input("Don't know which coffee to drink? Let me know how you're feeling:\n")
-
+def suggest_coffee(mood_input):
     messages = [
         {"role": "model", "parts": [{"text": react_system_prompt}]},
-        {"role": "user", "parts": [{"text": prompt}]}
+        {"role": "user", "parts": [{"text": mood_input}]}
     ]
 
     initial_response = generate_text_with_conversation(messages)
     initial_json = extract_json_from_text(initial_response)
+
     if initial_json.get("function_name") == "get_coffee_recepies":
         mood = initial_json["function_parms"]["mood"]
         coffee = get_coffee_recepies(mood)
@@ -23,10 +23,18 @@ def coffee_suggester_main():
         })
 
         final_response = generate_text_with_conversation(messages)
-        print(clean_answer_text(final_response))
-
+        return clean_answer_text(final_response)
     else:
-        print("Couldn't interpret mood. Try again!")
+        return "Couldn't interpret your mood. Try describing it differently!"
+
+# Gradio Interface
+iface = gr.Interface(
+    fn=suggest_coffee,
+    inputs=gr.Textbox(lines=2, placeholder="How are you feeling today?"),
+    outputs=gr.Textbox(label="Suggested Coffee"),
+    title="Coffee Suggestor ☕",
+    description="Tell me your mood, and I'll suggest the perfect coffee for you!"
+)
 
 if __name__ == "__main__":
-    coffee_suggester_main()
+    iface.launch()
